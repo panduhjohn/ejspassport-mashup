0610164090ae1d86c8d944ebc842e60f56cb3180
John Stilwell <johnrstilwell@codeimmersives.com>
2020-02-16 20:06:56 -0500

    added main and partials

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const chalk = require('chalk')
-const express = require('express')
-const app = express()
-const logger = require('morgan')
-const path = require('path')
-const fetch = require('node-fetch')
+const chalk = require('chalk');
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const path = require('path');
+const fetch = require('node-fetch');
+const axios = require('axios');
 
-require('dotenv').config()
+require('dotenv').config();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,19 +16,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.render('main/home')
-})
+    res.render('main/home');
+});
 app.get('/movies', (req, res) => {
-    res.render('main/home')
-})
+    res.render('main/movies');
+});
 app.get('/random', (req, res) => {
-    res.render('main/home')
-})
+    const url = 'https://randomuser.me/api/?results=1';
+    axios(url).then(results => console.log(results.data));
+    res.render('main/random');
+});
 
 app.listen(port, () => {
     console.log(chalk.blue(`App listening on port ${port}`));
-})
+});
