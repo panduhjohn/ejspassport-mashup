f47de0529ff86380a15421ccbd2b4a91ae2f7d0a
John Stilwell <johnrstilwell@codeimmersives.com>
2020-02-17 17:25:46 -0500

    got data to CL

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# ejspassport-mashup
-
 Create a repo called ejspassport-mashup. Please submit the repo here.
 
 -create a folder called ejspassport-mashup
@@ -26,5 +24,11 @@ Create a repo called ejspassport-mashup. Please submit the repo here.
 ---movie header should say Movies Now Playing
 
 ****movie and random footer should have copyright symbol and programmer name
+****STYLE RANDOM AND MOVIES WITH CSS AS REFERRED IN THE VIDEO OR HOWEVER YOU THINK WILL LOOK BETTER.
+
+APIs:
+randomuser.me - log 20 random users in ALPHABETICAL order giving image and full name.
+tmdb.org - show the most popular movies of 2019 with posters and synopsis
 
 Two videos are attached below to show what is wanted for this homework. Please download then watch
+ALSO MAKE A CHECKLIST FOR YOURSELF TO MAKE SURE YOU HAVE FINISHED DOING EVERYTHING REQUESTED!
--- index.js
+++ index.js
@@ -22,12 +22,22 @@ app.get('/', (req, res) => {
     res.render('main/home');
 });
 app.get('/movies', (req, res) => {
+    const key = process.env.MOVIE_API;
+    // const url = `https://api.themoviedb.org/3/movie/popular/?api_key=${key}`;
+    const url2 = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`;
+    axios(url2 + key)
+        .then(results => console.log(results.data))
+        .catch(err => console.log('Error in movies', err));
     res.render('main/movies');
 });
+
 app.get('/random', (req, res) => {
-    const url = 'https://randomuser.me/api/?results=1';
-    axios(url).then(results => console.log(results.data));
-    res.render('main/random');
+    const url = 'https://randomuser.me/api/?results=5';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log('Error in Random Call', err));
+    res.render('main/random', {data});
 });
 
 app.listen(port, () => {
