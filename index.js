aae7f00b9dbbbd5f380650bc985790681b4f5864
John Stilwell <johnrstilwell@codeimmersives.com>
2020-02-17 19:48:15 -0500

    clean up unused code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const logger = require('morgan');
 const path = require('path');
 const fetch = require('node-fetch');
-const axios = require('axios');
 
 require('dotenv').config();
 
@@ -18,26 +17,32 @@ app.set('views', path.join(__dirname, 'views'));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.render('main/home');
-});
 app.get('/movies', (req, res) => {
     const key = process.env.MOVIE_API;
-    // const url = `https://api.themoviedb.org/3/movie/popular/?api_key=${key}`;
-    const url2 = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`;
-    axios(url2 + key)
-        .then(results => console.log(results.data))
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`;
+    const img = 'https://image.tmdb.org/t/p/w500';
+    fetch(url + key)
+        .then(res => res.json())
+        .then(data => {
+            res.render('main/movies', { data, img });
+        })
         .catch(err => console.log('Error in movies', err));
-    res.render('main/movies');
 });
 
 app.get('/random', (req, res) => {
-    const url = 'https://randomuser.me/api/?results=5';
+    const url = 'https://randomuser.me/api/?results=20';
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log('Error in Random Call', err));
-    res.render('main/random', {data});
+        .then(users => {
+            // const sortUsers = users.results.sort((a,b) => a + b)
+            const sortUsers = users.results.sort((a, b) => {
+                if (b.name.last > a.name.last) return -1;
+                else if (a.name.last > b.name.last) return 1;
+                else return 0;
+            });
+            res.render('main/random', { sortUsers });
+        })
+        .catch(err => console.log('Error in Random', err));
 });
 
 app.listen(port, () => {
